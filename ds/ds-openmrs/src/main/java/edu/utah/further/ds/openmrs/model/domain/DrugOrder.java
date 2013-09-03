c5ee340c3bed0ebc435e8041e7e2c886a8d89d96
Dustin Schultz <dustin.schultz@utah.edu>
2013-09-03 14:43:45 -0600

    Add Apache License header and standard Class documentation header.

--- ds/ds-openmrs/src/main/java/edu/utah/further/ds/openmrs/model/domain/DrugOrder.java
+++ ds/ds-openmrs/src/main/java/edu/utah/further/ds/openmrs/model/domain/DrugOrder.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) [2013] [The FURTHeR Project]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package edu.utah.further.ds.openmrs.model.domain;
 
 import javax.persistence.Column;
@@ -17,6 +32,17 @@ import edu.utah.further.core.api.data.PersistentEntity;
 /**
  * The persistent class and data transfer object for the drug_order database table.
  * 
+ * <p>
+ * -----------------------------------------------------------------------------------<br>
+ * (c) 2008-2012 FURTHeR Project, Health Sciences IT, University of Utah<br>
+ * Contact: {@code <further@utah.edu>}<br>
+ * Biomedical Informatics, 26 South 2000 East<br>
+ * Room 5775 HSEB, Salt Lake City, UT 84112<br>
+ * Day Phone: 1-801-581-4080<br>
+ * -----------------------------------------------------------------------------------
+ * 
+ * @author N. Dustin Schultz {@code <dustin.schultz@utah.edu>}
+ * @version Sep 3, 2013
  */
 @Entity
 @Table(name = "drug_order")
